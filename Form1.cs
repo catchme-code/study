2a0685d815d41a43c2f34e267a9fedebe8dd946c
catchme-code <tntngusrl@naver.com>
2024-10-03 12:38:27 +0900

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace C_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String text = textBox1.Text;
+            
+
+            //오라클 연결
+            String conInfo = "Data Source=192.168.0.4:1521; User Id=C##test; Password=1234";
+            OracleConnection con = new OracleConnection(conInfo);
+            con.Open();
+
+            //slq 실행 준비
+                  
+            String sql = "insert into test values(:str)";
+
+            OracleCommand cmd = new OracleCommand(sql,con);
+
+            cmd.Parameters.Add(new OracleParameter("str",text));
+
+
+
+            //sql 실행
+            //cmd.ExecuteNonQuery(); -> select 이 외
+            //cmd.ExecuteReader -> select 일때
+            int odr = cmd.ExecuteNonQuery();
+
+            con.Close();
+     
+
+            Form2 f2 = new Form2();
+            f2.Show();
+            
+
+
+                
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,48 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace C_
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+       
+
+
+        private void Form2_Load_1(object sender, EventArgs e)
+        {
+            //오라클 연결
+            String conInfo = "Data Source=192.168.0.4:1521; User Id=C##test; Password=1234";
+            OracleConnection con = new OracleConnection(conInfo);
+            con.Open();
+
+
+            String sql = "select * from test";
+
+            OracleCommand cmd = new OracleCommand(sql, con);
+
+
+            OracleDataReader odr = cmd.ExecuteReader();
+            while (odr.Read()) {
+                listBox2.Items.Add(odr.GetString(0));
+            }
+
+            con.Close();
+
+           
+        }
+    }
+}
