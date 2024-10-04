e81c451302f55f70ccf87812a0bd3fa06ec38e7b
catchme-code <tntngusrl@naver.com>
2024-10-04 15:28:01 +0900

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -2,62 +2,46 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Oracle.ManagedDataAccess.Client;
 
-namespace C_
+namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        public static String uid = "db";
+        public static String password = "1234";
+        public static String database = "test_db";
+        public static String server = "192.168.0.17";
+
+
         public Form1()
         {
             InitializeComponent();
         }
-        
 
         private void button1_Click(object sender, EventArgs e)
         {
             String text = textBox1.Text;
+            Console.WriteLine(text);
+            String Constr = "SERVER=" + server + "; DATABASE=" + database + ";UID=" + uid + ";PASSWORD=" + password + ";";// 위의 선언한 변수 값 적용
+            //String Constr = "SERVER=localhost; DATABASE=test; UID=test; PASSWORD=1234;";
+            SqlConnection conn = new SqlConnection(Constr); // constr의 값을 conn으로 반환하여 넣어줌 
+            conn.Open();
+
+            String sql = $"insert into T1 values('{text}')";//쿼리문 
+            SqlCommand cmd = new SqlCommand(sql, conn);//new SqlCommand(쿼리문,연결문(SqlConnection에서 선언한 conn)))
+            int sdr = cmd.ExecuteNonQuery();//데이터 읽기
+
             
-
-            //오라클 연결
-            String conInfo = "Data Source=192.168.0.4:1521; User Id=C##test; Password=1234";
-            OracleConnection con = new OracleConnection(conInfo);
-            con.Open();
-
-            //slq 실행 준비
-                  
-            String sql = "insert into test values(:str)";
-
-            OracleCommand cmd = new OracleCommand(sql,con);
-
-            cmd.Parameters.Add(new OracleParameter("str",text));
-
-
-
-            //sql 실행
-            //cmd.ExecuteNonQuery(); -> select 이 외
-            //cmd.ExecuteReader -> select 일때
-            int odr = cmd.ExecuteNonQuery();
-
-            con.Close();
-     
-
+            conn.Close();
+            
             Form2 f2 = new Form2();
             f2.Show();
-            
-
-
-                
-            
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
 
         }
     }
--- Form2.cs
+++ Form2.cs
@@ -1,48 +1,50 @@
-﻿using Oracle.ManagedDataAccess.Client;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Net.Mime.MediaTypeNames;
 
-namespace C_
+namespace WindowsFormsApp2
 {
     public partial class Form2 : Form
     {
+        public static String uid = "db";
+        public static String password = "1234";
+        public static String database = "test_db";
+        public static String server = "192.168.0.17";
+
         public Form2()
         {
             InitializeComponent();
         }
 
-       
-
-
-        private void Form2_Load_1(object sender, EventArgs e)
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //오라클 연결
-            String conInfo = "Data Source=192.168.0.4:1521; User Id=C##test; Password=1234";
-            OracleConnection con = new OracleConnection(conInfo);
-            con.Open();
 
+        }
 
-            String sql = "select * from test";
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+            String Constr = "SERVER=" + server + "; DATABASE=" + database + ";UID=" + uid + ";PASSWORD=" + password + ";";// 위의 선언한 변수 값 적용
+            //String Constr = "SERVER=localhost; DATABASE=test; UID=test; PASSWORD=1234;";
+            SqlConnection conn = new SqlConnection(Constr); // constr의 값을 conn으로 반환하여 넣어줌 
+            conn.Open();
 
-            OracleCommand cmd = new OracleCommand(sql, con);
-
-
-            OracleDataReader odr = cmd.ExecuteReader();
-            while (odr.Read()) {
-                listBox2.Items.Add(odr.GetString(0));
+            String sql = "select * from T1";//쿼리문 
+            SqlCommand cmd = new SqlCommand(sql, conn);//new SqlCommand(쿼리문,연결문(SqlConnection에서 선언한 conn)))
+            SqlDataReader sdr = cmd.ExecuteReader();//데이터 읽기
+            while (sdr.Read()) {
+                listBox1.Items.Add(sdr.GetString(0));
             }
 
-            con.Close();
 
-           
+            conn.Close();
         }
     }
 }
